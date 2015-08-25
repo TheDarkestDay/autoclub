4445c8b65739316934698a110d826b98837358d5
thedarkestday <alexzund3r@yahoo.com>
2015-08-25 07:11:13 +0000

    Tests for cars are comig

--- drivers/tests.py
+++ drivers/tests.py
@@ -1,13 +1,16 @@
 from rest_framework import test, status
-from drivers.models import Driver
+from drivers.models import Driver,Car
 
 # Create your tests here.
-class DriversTestCase(test.APITestCase):
+class AutoclubTestCase(test.APITestCase):
     client = test.APIClient()
     driver_id = 1
+    car_id = 1
     def setUp(self):
-        res = self.client.post('/api/drivers/', {'name': 'Marina', 'age': 28,'profile': 'The last woman in the world'})
-        driver_id = res.data["id"]
+        self.client.post('/api/drivers/', {'name': 'Marina', 'age': 28,'profile': 'The last woman in the world'})
+        self.driver_id = Driver.objects.get().id
+        self.client.post('/api/cars/', {'model_name':'Jiguli','owner': self.driver_id})
+        self.car_id = Car.objects.get().id
     
     def test_can_create_driver(self):
         current_count = Driver.objects.all().count()
@@ -22,4 +25,8 @@ class DriversTestCase(test.APITestCase):
         new_count = Driver.objects.all().count()
         self.assertEqual(current_count-new_count,1)
         
+    def test_can_update_driver(self):
+        self.client.put('/api/drivers/%d/'%self.driver_id, {'name':'Katerina', 'age': 28, 'profile': 'The last woman in the world'})
+        updated_driver = Driver.objects.get()
+        self.assertEqual(updated_driver.name,'Katerina')
         
