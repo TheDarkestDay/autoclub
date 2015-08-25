c761d0f5343cd5a5082201be155c259c5e332640
thedarkestday <alexzund3r@yahoo.com>
2015-08-25 09:35:19 +0000

    Refactoring

--- drivers/tests.py
+++ drivers/tests.py
@@ -3,55 +3,38 @@ from drivers.models import Driver,Car
 
 # Create your tests here.
 class AutoclubTestCase(test.APITestCase):
-    client = test.APIClient()
-    driver_id = 1
-    car_id = 1
     def setUp(self):
-        self.client.post('/api/drivers/', {'name': 'Marina', 'age': 28,'profile': 'The last woman in the world'})
-        self.driver_id = Driver.objects.get().id
-        self.client.post('/api/cars/', {'model_name':'Jiguli','owner': self.driver_id})
-        self.car_id = Car.objects.get().id
-    
+        self.client = test.APIClient()
+        self.driver = Driver.objects.create(name='Marina', age=28, profile='The last woman in the world')
+        self.car = Car.objects.create(model_name='Jiguli', owner=self.driver)
+        self.client.post('/api/cars/', {'model_name':'Jiguli','owner': self.driver.id})
+
     def test_can_create_driver(self):
-        new_driver = Driver.objects.get(id=self.driver_id)
-        self.assertEqual(new_driver.name,'Marina')
-        self.assertEqual(new_driver.age, 28)
-        self.assertEqual(new_driver.profile,'The last woman in the world')
-        
+        resp = self.client.post('/api/drivers/', {'name': 'Marina', 'age': 28,'profile': 'The last woman in the world'})
+        self.assertEqual(status.HTTP_201_CREATED, resp.status_code)
+
+
     def test_can_delete_driver(self):
-        single_driver = Driver.objects.get(id=self.driver_id)
-        current_count = Driver.objects.all().count()
-        self.client.delete('/api/drivers/%d/'%self.driver_id)
-        new_count = Driver.objects.all().count()
-        self.assertEqual(current_count-new_count,1)
-        queryset = Car.objects.filter(owner=single_driver)
-        self.assertEqual(queryset.count(), 0)
-        
+        resp = self.client.delete('/api/drivers/%d/'%self.driver.id)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, resp.status_code)
+
     def test_can_update_driver(self):
-        self.client.put('/api/drivers/%d/'%self.driver_id, {'name':'Katerina', 'age': 28, 'profile': 'The last woman in the world'})
-        updated_driver = Driver.objects.get(id=self.driver_id)
+        resp = self.client.put('/api/drivers/%d/'%self.driver.id, {'name':'Katerina', 'age': 28, 'profile': 'The last woman in the world'})
+        updated_driver = Driver.objects.get(id=self.driver.id)
         self.assertEqual(updated_driver.name,'Katerina')
+        self.assertEqual(status.HTTP_200_OK, resp.status_code)
         
     def test_can_create_car(self):
-        car = Car.objects.get(id=self.car_id)
-        driver = Driver.objects.get(id=self.driver_id)
-        self.assertEqual(car.model_name,'Jiguli')
-        self.assertEqual(car.owner,driver)
+        resp = self.client.post('/api/cars/', {'model_name': 'Renault', 'owner': 1})
+        self.assertEqual(status.HTTP_201_CREATED,resp.status_code)
         
     def test_can_delete_car(self):
-        prev_count = Car.objects.all().count()
-        self.client.delete('/api/cars/%d/'%self.car_id)
-        new_count = Car.objects.all().count()
-        self.assertEqual(prev_count-new_count,1)
-        self.assertEqual(Car.objects.filter(model_name="Jiguli",owner=Driver.objects.get(id=self.driver_id)).exists(), False)
+        resp = self.client.delete('/api/cars/%d/'%self.car.id)
+        self.assertEqual(status.HTTP_204_NO_CONTENT,resp.status_code)
+        
         
     def test_can_update_car(self):
-        prev_count = Car.objects.all().count()
-        single_driver = Driver.objects.get(id=self.driver_id)
-        self.client.put('/api/cars/%d/'%self.car_id,{'model_name': 'Mercedez', 'owner': self.driver_id })
-        new_count = Car.objects.all().count()
-        self.assertEqual(new_count,prev_count)
-        car = Car.objects.get(id=self.car_id)
-        self.assertEqual(car.model_name,'Mercedez')
+        resp = self.client.put('/api/cars/%d/'%self.car.id,{'model_name': 'Mercedez', 'owner': self.driver.id })
+        self.assertEqual(status.HTTP_200_OK,resp.status_code)
     
         
